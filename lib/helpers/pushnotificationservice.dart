94019deca05162610170f3dafef0360305323601
trefl <marektrefler@gmail.com>
2020-12-29 12:31:37 +0100

    po 79

--- lib/helpers/pushnotificationservice.dart
+++ lib/helpers/pushnotificationservice.dart
@@ -3,30 +3,28 @@
 
 import 'dart:io';
 
+import 'package:cab_driver/datamodels/tripdetails.dart';
 import 'package:cab_driver/globalvariables.dart';
+import 'package:cab_driver/widgets/ProgressDialog.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PushNotificationService{
 
   final FirebaseMessaging fcm = FirebaseMessaging();
-  Future initialize() async {
+  Future initialize(context) async {
 
     fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-        String rideID = message['data']['ride_id'];
-        print('ride_id: $rideID');
+        fetchRideInfo(getRideID(message), context);
       },
       onLaunch: (Map<String, dynamic> message) async {
-        print("onLaunch: $message");
-        String rideID = message['data']['ride_id'];
-        print('ride_id: $rideID');
+        fetchRideInfo(getRideID(message), context);
       },
       onResume: (Map<String, dynamic> message) async {
-        print("onResume: $message");
-        String rideID = message['data']['ride_id'];
-        print('ride_id: $rideID');
+        fetchRideInfo(getRideID(message), context);
       },
 
     );
@@ -43,4 +41,57 @@ class PushNotificationService{
     fcm.subscribeToTopic('alldrivers');
     fcm.subscribeToTopic('allusers');
   }
+
+  String getRideID(Map<String, dynamic> message){
+    String rideID = '';
+    rideID = message['data']['ride_id'];
+    print('ride_id: $rideID');
+    return rideID;
+
+  }
+
+  void fetchRideInfo(String rideID, context){
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => ProgressDialog(status: 'Pobieranie informacji',),
+    );
+
+
+
+    DatabaseReference rideRef = FirebaseDatabase.instance.reference().child('rideRequest/$rideID');
+
+    rideRef.once().then((DataSnapshot snapshot){
+
+      Navigator.pop(context);
+
+      if(snapshot.value != null){
+        double pickupLat = double.parse(snapshot.value['location']['latitude'].toString());
+        double pickupLng = double.parse(snapshot.value['location']['longitude'].toString());
+        String pickupAddress = snapshot.value['pickup_address'].toString();
+
+        double destinationLat = double.parse(snapshot.value['destination']['latitude'].toString());
+        double destinationLng = double.parse(snapshot.value['destination']['longitude'].toString());
+        String destinationAddress = snapshot.value['destination_address'];
+        String paymentMethod = snapshot.value['payment_method'];
+        print("---------------------------pickupAddress:");
+        print(pickupAddress);
+        TripDetails tripDetails = TripDetails();
+        tripDetails.rideID = rideID;
+        tripDetails.pickupAddress = pickupAddress;
+        tripDetails.destinationAddress = destinationAddress;
+        tripDetails.pickup = LatLng(pickupLat, pickupLng);
+        tripDetails.destination = LatLng(destinationLat, destinationLng);
+        tripDetails.paymentMethod = paymentMethod;
+
+
+        print(tripDetails.destinationAddress);
+
+
+
+      }
+
+    });
+  }
+
 }
